6536dc99ad31f4e453492566cf49b50f3a48d038
Linh Vuong <mlvuong@gmail.com>
2011-08-23 20:57:15 -0700

    Dev Map up

--- Development.py
+++ Development.py
@@ -1,6 +1,6 @@
 import csv
-codereader = csv.reader(open('country-codes', 'r'), delimiter="\t")
-devreader = csv.reader(open('development_code', 'r'), delimiter="\t")
+codereader = csv.reader(open('country-codes.txt', 'r'), delimiter="\t")
+devreader = csv.reader(open('development_code.csv', 'rU'), delimiter=",")
 
 alpha3to2 = {}
 i = 0
@@ -12,9 +12,9 @@ i = 0
 next(devreader)
 for row in devreader:
 
-	if row[1] in alpha3to2 and row[6]:
+	if row[1] in alpha3to2 and row[2]:
 		alpha2 = alpha3to2[row[1]].lower()
-		pct = int(row[6])
+		pct = int(row[2])
 		if pct == 5:
 			fill = "#0868AC"
 		elif pct == 4:
